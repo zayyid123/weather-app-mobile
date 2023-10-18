2c840d71b71c213414a942d5e7987d49a8f9a4d9
zayyid123 <mochamadzayyid@gmail.com>
2023-10-18 10:45:19 +0700

    add lottie

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { Text, View } from 'react-native'
+import LottieView from 'lottie-react-native';
+import { StatusBar } from 'expo-status-bar';
 
 const App = () => {
   return (
     <View>
       <StatusBar style='light' translucent={false} backgroundColor='#000'/>
-      <Text>App</Text>
+      <View
+        className='flex-1 min-h-screen bg-[#323338]'
+      >
+        <Text>Hello</Text>
+      </View>
     </View>
   )
 }
