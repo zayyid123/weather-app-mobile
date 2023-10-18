e5da0b321e038e2dc20bc938daf0e4f37fcbadc4
zayyid123 <mochamadzayyid@gmail.com>
2023-10-18 10:00:35 +0700

    add status bar

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
+import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { Text, View } from 'react-native'
 
 const App = () => {
   return (
     <View>
+      <StatusBar style='light' translucent={false} backgroundColor='#000'/>
       <Text>App</Text>
     </View>
   )
